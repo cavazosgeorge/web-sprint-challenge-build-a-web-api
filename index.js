119df27729cc8b3845150dd20d2bb57f9c1c462d
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-29 19:14:03 -0400

    Updated routes path

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const app = express();
 
 const dotenv = require("dotenv");
 
+const errorMiddleware = require("./api/middlewares/errors");
+const ErrorHandler = require("./utils/errorHandler");
+
 // SETTING UP CONFIG.ENV FILE VARIABLES
 dotenv.config({
   path: "./config/config.env",
 });
 
 // HANDLING UNCAUGHT EXCEPTION ERRORS
-// process.on("uncaughtException", (err) => {
-//   console.log(`ERROR: ${err.message}`);
-//   console.log("Shutting down the server due to uncaught exception");
-//   process.exit(1);
-// });
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
+});
 
 // SETUP BODY PARSER
 app.use(express.json());
@@ -26,9 +29,14 @@ const projects = require("./api/projects/projects-router");
 app.use("/api", projects);
 
 // HANDLE UNHANDLED ROUTES
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+});
 
 // MIDDLEWARE => HANDLE ERRORS
+app.use(errorMiddleware);
 
+// SERVER INFORMATION
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
