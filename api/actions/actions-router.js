e6cde7c3d018454932eb2f300853c5e3bc352cc8
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-29 19:22:41 -0400

    Actions Endpoints created

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -27,3 +27,30 @@ router.post("/", validateBody, (req, res, next) => {
     })
     .catch(next);
 });
+
+router.put("/:id", validateId, (req, res, next) => {
+  Actions.update(req.params.id, req.body)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch(next);
+});
+
+router.delete("/:id", validateId, (req, res, next) => {
+  Actions.remove(req.params.id)
+    .then(() => {
+      res.status(200).json();
+    })
+    .catch(next);
+});
+
+// eslint-disable-next-line
+router.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(err.status || 500.json({
+        message : err.message,
+        customMessage : "Something here"
+    }))
+})
+
+module.exports = router;
