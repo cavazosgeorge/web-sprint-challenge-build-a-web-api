c5273de4888e68cc4b13ca4d1e710d6f31d51149
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-29 19:19:22 -0400

    GET: endpoints created

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,29 @@
 // Write your "actions" router here!
+const express = require("express");
+const { validateId, validateBody } = require("./actions-middlware");
+const router = express.router();
+const Actions = require("./actions-model");
+
+router.get("/", (req, res, next) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(next);
+});
+
+router.get("/:id", validateId, (req, res, next) => {
+  Actions.get(req.params.id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch(next);
+});
+
+router.post("/", validateBody, (req, res, next) => {
+  Actions.insert(req.body)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(next);
+});
