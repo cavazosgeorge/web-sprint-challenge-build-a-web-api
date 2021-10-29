3e05ecc789ffaa24ef84ebb4fd86faab829d64af
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-29 19:14:37 -0400

    Projects middleware complete

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,33 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model");
+
+function validateId(req, res, next) {
+  const { id } = req.params;
+  Projects.get(id)
+    .then((possibleProject) => {
+      if (possibleProject) {
+        req.user = possibleProject;
+        next();
+      } else {
+        next({
+          success: false,
+          message: "not found",
+          status: 404,
+        });
+      }
+    })
+    .catch(next);
+}
+
+function validateBody(req, res, next) {
+  if (!req.body.name || !req.body.description || req.body.completed === null) {
+    next({
+      success: false,
+      message: "missing required text field",
+    });
+  } else {
+    next();
+  }
+}
+
+module.exports = { validateId, validateBody };
