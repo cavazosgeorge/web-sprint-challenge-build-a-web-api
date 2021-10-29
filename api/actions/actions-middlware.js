b0ea6981e31e83441fb878910cd1ea33a89b8dc0
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-29 19:15:18 -0400

    Actions middleware completed

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,27 @@
 // add middlewares here related to actions
+const actions = require("./actions-model");
+
+function validateId(req, res, next) {
+  const { id } = req.params;
+  actions
+    .get(id)
+    .then((possibleProject) => {
+      if (possibleProject) {
+        req.user = possibleProject;
+        next();
+      } else {
+        next({ message: "not found", status: 404 });
+      }
+    })
+    .catch(next);
+}
+
+function validateBody(req, res, next) {
+  if (!req.body.notes || !req.body.description || !req.body.project_id) {
+    next({ status: 400, message: "missing required text field" });
+  } else {
+    next();
+  }
+}
+
+module.exports = { validateId, validateBody };
