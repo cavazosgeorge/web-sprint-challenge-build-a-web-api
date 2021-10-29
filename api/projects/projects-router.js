c7db6526188a1e7bb0145fe306fa06faacac7626
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-29 17:52:04 -0400

    GET: Endpoint created => /API/PROJECTS

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
 // Write your "projects" router here!
+const Projects = require("../projects/projects-model");
+
+// METHOD(GET) => /API/PROJECTS
+exports.getProjects = async (req, res, next) => {
+  const projects = await Projects.find();
+
+  res.status(200).json({
+    success: true,
+    results: projects.length,
+    data: projects,
+  });
+};
+
+// METHOD(GET) => /API/PROJECTS/:ID
+
+// METHOD(POST) => /API/PROJECTS
+
+// METHOD(PUT) => /API/PROJECTS/:ID
+
+// METHOD(DELETE) => /API/PROJECTS/:ID
+
+// METHOD(GET) => /API/PROJECTS/:ID/ACTIONS
